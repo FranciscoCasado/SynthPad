068a8bbda1c8fd7b66e5d7ede095d7bdc543bc7e
kenzo <knzo@live.cl>
2016-04-07 16:34:25 -0300

    Finished SAW osc. Tested.

--- oscilators/saw_osc.vhd
+++ oscilators/saw_osc.vhd
@@ -32,37 +32,23 @@ end saw_osc;
 architecture Behavioral of saw_osc is
 
   signal counter : unsigned(6 downto 0);
-  signal counter2 : unsigned(2 downto 0);
-
+  signal wave    : unsigned(9 downto 0);
+  
 begin
   
-  wave_out <= STD_LOGIC_VECTOR(counter)&"000";
-  
-  -- aqui falta separation of concerns.
-  -- counter siempre cuenta 100 para volver a 0
-  -- waveout debe hacer la pega, a dormir
+  wave_out <= STD_LOGIC_VECTOR(wave);
+  wave     <= (counter & "000") + ("00" & counter & "0");
   
   process(clk, tick, reset)
   begin
     if(reset = '1') then
       counter  <= (others => '0');
-      counter2 <= (others => '0');
     elsif(clk'event and clk = '1' and tick = '1') then
-      
-      if(counter2 = 5) then
-        counter2 <= (others => '0');
-      else
-        counter2 <= counter2 + 1;
-      end if;
-    
+     
       if(counter = 99) then
         counter <= (others => '0');
       else
-        if(counter2 = 5) then
-          counter <= counter + 2;
-        else
-          counter <= counter + 1;
-        end if;
+        counter <= counter + 1;
       end if;
     end if;
   end process;
