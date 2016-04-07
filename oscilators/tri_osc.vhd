dd83f69fa60755e31ee3574ffa53f90af7a4ca80
kenzo <knzo@live.cl>
2016-04-07 18:00:36 -0300

    Triangle oscilator works. Tested

--- oscilators/tri_osc.vhd
+++ oscilators/tri_osc.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date:    19:36:57 04/06/2016 
 -- Design Name: 
--- Module Name:    square_osc - Behavioral 
+-- Module Name:    tri_osc - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -31,24 +31,50 @@ end tri_osc;
 
 architecture Behavioral of tri_osc is
 
+  signal next_counter : unsigned(5 downto 0);
   signal counter : unsigned(5 downto 0);
-  signal state : std_logic;
+  signal wave    : unsigned(9 downto 0);
+  signal state   : std_logic;
+  signal next_state : std_logic;
+  
 begin
   
-  wave_out <= STD_LOGIC_VECTOR(counter);
+  wave_out <= STD_LOGIC_VECTOR(wave);
   
+  wave     <= (counter & "0000") + ("00" & counter & "00");
+
   process(clk, tick, reset)
   begin
     if(reset = '1') then
       counter <= (others => '0');
       state   <= '0';
     elsif(clk'event and clk = '1' and tick = '1') then
-      if(counter = 49) then
-        counter <= (others => '0');
-      else
-        counter <= counter + 1;
-      end if;
+      counter <= next_counter;
+      state   <= next_state;
     end if;
   end process;
 
+  -- next state logic
+  process(counter, state)
+  begin
+    if(counter = 1) then
+      next_state <= '0';
+    elsif(counter = 49) then
+      next_state <= '1';
+    else
+      next_state <= state;
+    end if;
+  end process;
+
+  -- next counter logic
+  process(state, counter)
+  begin
+    if(state = '0') then
+      next_counter <= counter + 1;
+    elsif(state = '1') then
+      next_counter <= counter - 1;
+    else
+      next_counter <= counter;
+    end if;
+  end process;
 end Behavioral;
---
+++ oscilators/tri_osc_test.vhd
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   16:42:09 04/07/2016
+-- Design Name:   
+-- Module Name:   C:/Users/Kenzo/Desktop/Synthpad/oscilators/tri_osc_test.vhd
+-- Project Name:  oscilators
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: tri_osc
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+ 
+entity tri_osc_test is
+end tri_osc_test;
+ 
+architecture behavior of tri_osc_test is 
+ 
+  -- Component Declaration for the Unit Under Test (UUT)
+ 
+  component tri_osc
+  port(
+    clk      : in  std_logic;
+    reset    : in  std_logic;
+    tick     : in  std_logic;
+    wave_out : out std_logic_vector(9 downto 0)
+  );
+  end component;
+    
+
+  --Inputs
+  signal clk   : std_logic := '0';
+  signal reset : std_logic := '0';
+  signal tick  : std_logic := '0';
+
+  --Outputs
+  signal wave_out : std_logic_vector(9 downto 0);
+
+  -- Clock period definitions
+  constant clk_period : time := 10 ns;
+ 
+begin
+ 
+  -- Instantiate the Unit Under Test (UUT)
+  uut: tri_osc 
+  port map(
+    clk      => clk,
+    reset    => reset,
+    tick     => tick,
+    wave_out => wave_out
+  );
+
+  -- Clock process definitions
+  clk_process :process
+  begin
+  clk <= '0';
+  wait for clk_period/2;
+  clk <= '1';
+  wait for clk_period/2;
+  end process;
+ 
+  -- Stimulus process
+  stim_proc: process
+  begin		
+    -- hold reset state for 100 ns.
+    wait for 100 ns;	
+    reset <= '1';
+    wait for clk_period*10;
+    tick  <= '1';
+    reset <= '0';
+    -- insert stimulus here 
+
+    wait;
+  end process;
+
+end;
